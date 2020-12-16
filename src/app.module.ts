e1a2aa99522d934ed4dffc347cd361e38d94534a
jstevick+1-at-666254836729 <joelstevick@outlook.com>
2020-12-15 19:40:25 -0500

    configuration for implementing-service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { ListService } from './list.service';
+import { GraphQLFederationModule } from '@nestjs/graphql';
+import { ListResolvers } from './resolver';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLFederationModule.forRoot({
+      autoSchemaFile: true,
+    }),
+  ],
   controllers: [],
-  providers: [ListService],
+  providers: [ListService, ListResolvers],
 })
 export class AppModule {}
